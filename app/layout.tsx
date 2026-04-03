8291a52d3ddf217ac44e17e4a3902bcb9c6534d3
Fahim <fahim@sentri>
2026-04-03 17:24:21 +0000

    typography: replace Syne with Clash Display for all title/display headings

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,8 +25,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           {/* eslint-disable-next-line @next/next/no-page-custom-font */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+          <link rel="preconnect" href="https://api.fontshare.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Syne:wght@400;500;600;700;800&family=Onest:wght@300;400;500;600&family=JetBrains+Mono:wght@400;500&display=swap"
+            href="https://api.fontshare.com/v2/css?f[]=clash-display@400,500,600,700&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Onest:wght@300;400;500;600&family=JetBrains+Mono:wght@400;500&display=swap"
             rel="stylesheet"
           />
         </head>
